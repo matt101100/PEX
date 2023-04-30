cb826bc4fee694290d4fc9f7dbdbebad255159d9
matt <mlaz7837@uni.sydney.edu.au>
2023-04-30 08:44:43 +0000

    s

--- pe_common.h
+++ pe_common.h
@@ -11,6 +11,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 #define FIFO_EXCHANGE "/tmp/pe_exchange_%d"
 #define FIFO_TRADER "/tmp/pe_trader_%d"
@@ -19,5 +20,34 @@
 #define FILEPATH_LEN 100 // enough space to hold any filepath string
 #define MESSAGE_LEN 100
 #define PRODUCT_STR_LEN 17 // + 1 for null terminator
+
+// flags representing connection types to named pipes
+enum connection_type {
+    READ = 0,
+    WRITE
+};
+
+/*
+ * Desc: SIGUSR1 handler -- trader reads from the pe_exchange_*
+         FIFO in order to obtain the message sent by pe_exchange
+ * Params: The int representing SIGUSR1, which we handle
+ */
+void sigusr1_handle(int signum);
+
+/*
+ * Desc: Creates and initializes the sigaction struct, setting the handler to
+         my custom sigusr1 handler
+ * Return: The sigaction struct
+ */
+struct sigaction initialize_signal_action(void);
+
+/*
+ * Desc: Formats the file path with the trader's corresponding trader ID and 
+         opens the named pipe at that location.
+ * Params: Flag representing the type of connection the pipe will be opened for
+           and the trader ID
+ * Return: The file descriptor associated with the named pipe
+ */
+int connect_to_named_pipe(int connection_type, int trader_id);
 
 #endif
--- pe_exchange.h
+++ pe_exchange.h
@@ -5,4 +5,29 @@
 
 #define LOG_PREFIX "[PEX]"
 
+/*
+ * Desc: Generic order struct.
+ * Fields: string representing the product type of the order, and ints for the
+           quantity of the product and the price per unit. Also has a pointer
+           to the next order in the list.
+ */
+typedef struct order order;
+struct order {
+    char product[PRODUCT_STR_LEN];
+    int quantity;
+    int price;
+    order *next;
+};
+
+/*
+ * Desc: Order book struct.
+ * Fields: Holds pointers to the sell_orders struct, which contains all current
+           buy orders and similarly hold a pointer to the sell_orders struct.
+ */
+typedef struct order_book order_book;
+struct order_book {
+    order *buy_orders;
+    order *sell_orders;
+};
+
 #endif
