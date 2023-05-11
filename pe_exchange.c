9ce720ba56b540b8116c207670659a25c83c75e1
matt <mlaz7837@uni.sydney.edu.au>
2023-05-11 10:13:29 +0000

    s

--- pe_exchange.c
+++ pe_exchange.c
@@ -381,7 +381,7 @@ int execute_command(trader *curr_trader, char *message_in, int cmd_type, product
 			return 1;
 		}
 		snprintf(accepted_msg, msg_len + 1, "ACCEPTED %d;", order_id);
-		write(curr_trader->fd[1], "ACCEPTED 0;", strlen("ACCEPTED 0;"));
+		write(curr_trader->fd[1], "ACCEPTED 0;", strlen("ACCEPTED 0;") + 1);
 		kill(curr_trader->process_id, SIGUSR1);
 		free(accepted_msg);
 
