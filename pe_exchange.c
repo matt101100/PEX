ece50306f0706ec2f5d47c9fa87c8128d3f9460d
matt <mlaz7837@uni.sydney.edu.au>
2023-05-11 07:52:02 +0000

    s

--- pe_exchange.c
+++ pe_exchange.c
@@ -126,10 +126,6 @@ void signal_handle(int signum, siginfo_t *info, void *context) {
 	if (signum == SIGUSR1) {
 		// handle SIGUSR1
 		sigusr1 = 1;
-	} else if (signum == SIGCHLD) {
-		// handle SIGCHLD
-		printf("here\n");
-		sigchld = 1;
 	}
 
 	pid = info->si_pid;
