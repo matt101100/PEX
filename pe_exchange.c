27c4d815eb9a096ebc929a2db455fdd1df353872
matt <mlaz7837@uni.sydney.edu.au>
2023-05-11 13:33:13 +0000

    s

--- pe_exchange.c
+++ pe_exchange.c
@@ -126,6 +126,7 @@ void signal_handle(int signum, siginfo_t *info, void *context) {
 	if (signum == SIGUSR1) {
 		// handle SIGUSR1
 		sigusr1 = 1;
+		printf("here\n");
 		usleep(1);
 	} else if (signum == SIGCHLD) {
 		// handle SIGCHLD
