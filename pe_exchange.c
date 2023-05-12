a54718528f8ca861f1818f4e8e4038c3a0b361a6
matt <mlaz7837@uni.sydney.edu.au>
2023-05-12 08:34:48 +0000

    s

--- pe_exchange.c
+++ pe_exchange.c
@@ -201,7 +201,7 @@ int spawn_and_communicate(int num_traders, char **argv, trader **head) {
 	int trader_path_len = 0;
 	char *exchange_fifo_path = NULL;
 	char *trader_fifo_path = NULL;
-	// trader *prev = *head;
+	trader *prev = *head;
 	pid_t forked_pid = -1;
 	for (trader_id = 0; trader_id < num_traders; trader_id++) {
 		// get the length of each path
@@ -268,17 +268,16 @@ int spawn_and_communicate(int num_traders, char **argv, trader **head) {
 		new_trader->process_id = forked_pid;
 
 		// add the newly opened trader to the head of the list
+		new_trader->next = NULL;
 		if (*head == NULL) {
 			*head = new_trader;
 		} else {
 			// prev->next = new_trader;
-			// new_trader->next = NULL;
 			// prev = new_trader;
 			trader *current = *head;
 			while (current->next != NULL) {
 				current = current->next;
 			}
-			current->next = new_trader;
 		}
 		// new_trader->next = *head;
 		// *head = new_trader;
