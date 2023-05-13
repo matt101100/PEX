21ceed77219311b8388c7175667d1a5aa1e05760
matt <mlaz7837@uni.sydney.edu.au>
2023-05-13 11:54:04 +0000

    S

--- pe_exchange.c
+++ pe_exchange.c
@@ -607,7 +607,7 @@ float find_matches(int ****matches, order ***buys, order ***sells, trader *head,
 				// compute fee of the trade
 				trading_fee = trading_sum * FEE_PERCENTAGE;
 				long rounding = (long)(trading_fee + 0.5f);
-				trading_fee = (float)(rounding); // rounded to nearest decimal
+				trading_fee += (float)(rounding); // rounded to nearest decimal
 
 				// reduce the amount of product avaliable for this sell order
 				prod_sells->quantity -= prod_buys->quantity;
@@ -658,7 +658,7 @@ float find_matches(int ****matches, order ***buys, order ***sells, trader *head,
 				// compute fee of the trade
 				trading_fee = trading_sum * FEE_PERCENTAGE;
 				long rounding = (long)(trading_fee + 0.5f);
-				trading_fee = (float)(rounding);
+				trading_fee += (float)(rounding);
 
 				// cache the details of the trade
 				(*matches)[prod_buys->trader_id][product_index][0] += prod_buys->quantity;
@@ -714,10 +714,10 @@ float find_matches(int ****matches, order ***buys, order ***sells, trader *head,
 				// compute fee of the trade
 				trading_fee = trading_sum * FEE_PERCENTAGE;
 				long rounding = (long)(trading_fee + 0.5f);
-				trading_fee = (float)(rounding);
+				trading_fee += (float)(rounding);
 
 				// cache the details of the trade
-				(*matches)[prod_buys->trader_id][product_index][0] += prod_buys->quantity;
+				(*matches)[prod_buys->trader_id][product_index][0] += prod_sells->quantity;
 				(*matches)[prod_buys->trader_id][product_index][1] -= trading_sum;
 				(*matches)[prod_sells->trader_id][product_index][0] -= prod_sells->quantity;
 				(*matches)[prod_sells->trader_id][product_index][1] += (long)(trading_sum - trading_fee);
