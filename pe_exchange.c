3719066f6c358568e2674d8c6bd3afb2804d38da
matt <mlaz7837@uni.sydney.edu.au>
2023-05-13 13:41:43 +0000

    S

--- pe_exchange.c
+++ pe_exchange.c
@@ -752,8 +752,8 @@ void find_matches(int ****matches, order ***buys, order ***sells, trader *head, 
 
 				// print the results of the trade to stdout
 				printf("%s Match: Order %d [T%d], New Order %d [T%d], value: $%ld, fee: $%.0f.\n",
-				 		LOG_PREFIX, prod_buys->order_id, prod_buys->trader_id, 
-						prod_sells->order_id, prod_sells->trader_id, 
+				 		LOG_PREFIX, prod_sells->order_id, prod_buys->trader_id, 
+						prod_sells->order_id, prod_buys->trader_id, 
 						trading_sum, trading_fee);
 
 				// send fill messages to traders involved
