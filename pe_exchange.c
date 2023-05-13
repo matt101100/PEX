94cbf3050ee35c6bd69a6335801fae8400a2cc5d
matt <mlaz7837@uni.sydney.edu.au>
2023-05-13 09:17:01 +0000

    s

--- pe_exchange.c
+++ pe_exchange.c
@@ -612,11 +612,11 @@ float find_matches(int ****matches, order ***buys, order ***sells, trader *head,
 			if (prod_buys->quantity < prod_sells->quantity) {
 				// compute fee of the trade
 				trading_fee = trading_sum * FEE_PERCENTAGE;
-				long rounding = (long)(trading_fee + 0.5f);// tes
+				long rounding = (long)(trading_fee + 0.5f);
 				trading_fee = (float)(rounding); // rounded to nearest decimal
 
 				// reduce the amount of product avaliable for this sell order
-				prod_sells->quantity -= prod_buys->quantity;
+				// prod_sells->quantity -= prod_buys->quantity;
 
 				// cache the details of the trade
 				(*matches)[prod_buys->trader_id][product_index][0] += prod_buys->quantity;
