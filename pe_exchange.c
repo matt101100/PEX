9823036231c9ef38c4743dae183408fb4a332143
matt <mlaz7837@uni.sydney.edu.au>
2023-05-14 01:58:07 +0000

    S

--- pe_exchange.c
+++ pe_exchange.c
@@ -824,8 +824,8 @@ void find_matches(int ****matches, order ***buys, order ***sells, trader *head, 
 
 				// compute fee of the trade
 				trading_fee = trading_sum * FEE_PERCENTAGE;
-				long rounding = (long)(trading_fee + 0.5f);
-				trading_fee = (double)(rounding); // rounded to nearest decimal
+				// long rounding = (long)(trading_fee + 0.5f);
+				// trading_fee = (double)(rounding); // rounded to nearest decimal
 
 				// update the total trading fees sum
 				*total_trading_fees += trading_fee;
@@ -900,8 +900,8 @@ void find_matches(int ****matches, order ***buys, order ***sells, trader *head, 
 
 				// compute fee of the trade
 				trading_fee = trading_sum * FEE_PERCENTAGE;
-				long rounding = (long)(trading_fee + 0.5f);
-				trading_fee = (double)(rounding);
+				// long rounding = (long)(trading_fee + 0.5f);
+				// trading_fee = (double)(rounding);
 
 				// update the total trading fees sum
 				*total_trading_fees += trading_fee;
@@ -981,8 +981,8 @@ void find_matches(int ****matches, order ***buys, order ***sells, trader *head, 
 
 				// compute fee of the trade
 				trading_fee = trading_sum * FEE_PERCENTAGE;
-				long rounding = (long)(trading_fee + 0.5f);
-				trading_fee = (double)(rounding);
+				// long rounding = (long)(trading_fee + 0.5f);
+				// trading_fee = (double)(rounding);
 
 				// update the total trading fees sum
 				*total_trading_fees += trading_fee;
